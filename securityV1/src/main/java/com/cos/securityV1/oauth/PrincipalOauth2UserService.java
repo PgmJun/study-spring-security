6b3026a955d0f16e1536bd4aa337c948ef751323
JUN <chltmdwns96@naver.com>
2023-06-28 00:52:07 +0900

    Spring Security & JWT 학습 - 11강
    네이버 로그인 및 자동 회원가입 진행 완료

--- securityV1/src/main/java/com/cos/securityV1/oauth/PrincipalOauth2UserService.java
+++ securityV1/src/main/java/com/cos/securityV1/oauth/PrincipalOauth2UserService.java
@@ -4,6 +4,7 @@ import com.cos.securityV1.auth.PrincipalDetails;
 import com.cos.securityV1.model.User;
 import com.cos.securityV1.oauth.provider.FacebookUserInfo;
 import com.cos.securityV1.oauth.provider.GoogleUserInfo;
+import com.cos.securityV1.oauth.provider.NaverUserInfo;
 import com.cos.securityV1.oauth.provider.OAuth2UserInfo;
 import com.cos.securityV1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -43,8 +45,12 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
-        } else {
-            System.out.println("우리는 구글과 페이스북만 지원해요");
+        } else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
+        }
+        else {
+            System.out.println("우리는 구글과 페이스북과 네이버만 지원해요");
         }
 
 
